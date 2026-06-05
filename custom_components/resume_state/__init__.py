3044c236c0a1f4c589bbe6d8840b94c276af74d5
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2026-06-04 20:52:39 -0500

    Fixing workflows and fixing lint issues

--- custom_components/resume_state/__init__.py
+++ custom_components/resume_state/__init__.py
@@ -1,16 +1,19 @@
 """The Resume State component."""
 
-import logging
-from typing import Any
+from __future__ import annotations
 
-from homeassistant import core
+import logging
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from homeassistant import core
 
 from .const import DOMAIN, SERVICE_HELLO_WORLD
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup(hass: core.HomeAssistant, config: dict[str, Any]) -> bool:
+async def async_setup(hass: core.HomeAssistant, _config: dict[str, Any]) -> bool:
     """Set up the Resume State component."""
 
     async def async_hello_world(_call: core.ServiceCall) -> None:
