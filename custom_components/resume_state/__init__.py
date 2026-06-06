0d69fdad87f6d702d7cb8d62b976336b3fe85167
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2026-06-05 23:42:48 -0500

    adding config and splitting out services (#14)

--- custom_components/resume_state/__init__.py
+++ custom_components/resume_state/__init__.py
@@ -5,21 +5,31 @@ from __future__ import annotations
 import logging
 from typing import TYPE_CHECKING, Any
 
+from .config import CONF_DELAY, CONF_ENTITIES
+from .services import async_setup_services
+
 if TYPE_CHECKING:
     from homeassistant import core
 
-from .const import DOMAIN, SERVICE_HELLO_WORLD
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass: core.HomeAssistant, _config: dict[str, Any]) -> bool:
     """Set up the Resume State component."""
+    conf = _config.get(DOMAIN)
 
-    async def async_hello_world(_call: core.ServiceCall) -> None:
-        """Handle the hello_world service call."""
-        _LOGGER.info("Hello World")
+    if conf is None:
+        _LOGGER.error("No configuration found for Resume State")
+        return False
 
-    hass.services.async_register(DOMAIN, SERVICE_HELLO_WORLD, async_hello_world)
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN] = {
+        CONF_ENTITIES: conf.get(CONF_ENTITIES),
+        CONF_DELAY: conf.get(CONF_DELAY),
+    }
+
+    await async_setup_services(hass)
 
     return True
