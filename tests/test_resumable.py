5f8b2f331c1b95edc816e2949568d329fd7a0c4d
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2026-06-26 22:54:10 -0500

    lint

--- tests/test_resumable.py
+++ tests/test_resumable.py
@@ -1,15 +1,15 @@
 """Tests for the resumable package."""
 
-from homeassistant.core import HomeAssistant, State
+from homeassistant.core import State
 
 from custom_components.resume_state.resumable import build_resumable_entity
 
 
-def test_build_resumable_entity_unsupported_domain(hass: HomeAssistant) -> None:
+def test_build_resumable_entity_unsupported_domain() -> None:
     """Test build_resumable_entity with an unsupported domain."""
     entity_id = "switch.test"
     state = State(entity_id, "on")
 
-    entity = build_resumable_entity(hass, entity_id, state)
+    entity = build_resumable_entity(entity_id, state)
 
     assert entity is None
