b1d02b8d06ce841fb49ddb0fb02526c4c0c681ec
frozenwizard <172203+frozenwizard@users.noreply.github.com>
2026-06-26 22:33:21 -0500

    Make an entity and entity, it now takes a hass object

--- custom_components/resume_state/resumable/__init__.py
+++ custom_components/resume_state/resumable/__init__.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 from custom_components.resume_state.resumable.resumable_light import ResumableLight
 
 if TYPE_CHECKING:
-    from homeassistant.core import HomeAssistant, State
+    from homeassistant.core import State
 
     from custom_components.resume_state.resumable.resumable_entity import (
         ResumableEntity,
@@ -13,9 +13,9 @@ if TYPE_CHECKING:
 
 
 def build_resumable_entity(
-    hass: HomeAssistant, entity_id: str, previous_state: State
+    entity_id: str, previous_state: State
 ) -> ResumableEntity | None:
     """Build a resumable entity from a previous state."""
     if entity_id.startswith("light."):
-        return ResumableLight(hass, entity_id, previous_state)
+        return ResumableLight(entity_id, previous_state)
     return None
